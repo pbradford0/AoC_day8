019290df5f108b7b092d27c4f330597e26b94886
pbradford0 <phil.w.bradford@gmail.com>
2016-11-15 19:44:01 -0500

    part 1 done

--- day8.py
+++ day8.py
@@ -12,13 +12,13 @@ def difference_calc(filename):
   line_count = 0
   #create regex to filter out what isn't put into memory:
   #1) start and end quotes
-  re_quotes = re.compile("\".*\"")
+  re_quotes = re.compile("[\"].*[\"]")
   #2) \\
-  re_backslash = re.compile(".*\\\\")
+  re_backslash = re.compile("[\\\\][\\\\]")
   #3) \"
-  re_quote = re.compile(".*\".*")
+  re_quote = re.compile("[\\\\][\\\"]")
   #4) \x00 (where 00 is any hexadecimal pair)
-  re_hex = re.compile(".*\\[x][0-9a-f][0-9a-f].*")
+  re_hex = re.compile("[\\\\]x[0-9a-f][0-9a-f]")
   
   input = open(filename, 'rU')
   for line in input:
@@ -34,11 +34,28 @@ def difference_calc(filename):
       print "Error: Data contains a non-string"
       break
     else:
-      line_count -= 2
-      
-    #check regex 2. can have multiple matches, 
-    line_count -= len(re.findall(re_backslash, line))
+      line = line[1:-1]
+    
+    print "Before: "+ line,
+    #check regex 2. can have multiple matches
+    line = re.sub(re_backslash, "\\\\", line)
+    #check regex 3, same^
+    line = re.sub(re_quote, "\"", line)
+    #check regex 4
+    line = re.sub(re_hex, "H", line)
+    #debug
+    #debug block
+    print " and after: "+ line
+    #print "line: " + str(line_count+2),
+    #print "| backslash: " + str(len(re.sub(re_backslash, line))),
+    #print "| quote: "+ str(len(re.sub(re_quote, line))),
+    #print "| hex: "+ str(3*(len(re.sub(re_hex, line)))),
+    
+    #print "| total: " + str(line_count)
+    #recalc line_count for the subbed string
+    line_count = len(line)
     mem_count += line_count
+    
   diff = file_count - mem_count
   return diff
 
